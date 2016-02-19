8f201718c94c0f486f3eab010fc2374c5f862bba
Andrew "Akari" Alexeyew <akari@dbc.1gb.ua>
2016-02-19 23:59:41 +0200

    Revamped the weapon model cvar system and added support for attachment offset cvars

---
+++ extras/scripts/make-weaponmodel-cvars.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+import re
+
+from pathlib import Path
+
+
+defaults = {
+    "laser"             :   "laser",
+    "shotgun"           :   "rm_shotgun",
+    "uzi"               :   "uzi",
+    "grenadelauncher"   :   "gl",
+    "electro"           :   "electro",
+    "crylink"           :   "crylink",
+    "nex"               :   "nex",
+    "hagar"             :   "hagar",
+    "rocketlauncher"    :   "rl",
+    "porto"             :   "porto",
+    "minstanex"         :   "minstanex",
+    "hook"              :   "hookgun",
+    "hlac"              :   "hlac",
+    "tuba"              :   "tuba",
+    "campingrifle"      :   "rm_campingrifle",
+    "fireball"          :   "fireball",
+    "zapper"            :   "zapper",
+    "flak"              :   "flak",
+    "pryaxe"            :   "pryaxe",
+}
+
+
+weapon_regexp = re.compile(
+    r'\s*register_weapon\((?:.*?,){8}\s*"(?P<sname>.*?)"\s*,\s*"(?P<wname>.*?)"\s*,.*?,.*?\)\s*'
+)
+
+
+def main():
+    with (Path(__file__).absolute().parent.parent.parent / "qcsrc/common/items.qc").open() as f:
+        for line in f:
+            m = weapon_regexp.match(line)
+            if m:
+                fargs = {g: m.group(g) for g in ("wname", "sname")}
+                fargs["default"] = defaults.get(fargs["sname"], fargs["sname"])
+                fargs["default_ofs"] = defaults.get(fargs["sname"] + "_ofs", "0 0 0")
+
+                print('set sv_weaponmodel_%(sname)s "%(default)s" "Weapon model replacement for the %(wname)s"\n'
+                      'set sv_weaponmodel_%(sname)s_offset "%(default_ofs)s" "Weapon model offset for the %(wname)s"\n' % fargs)
+
+
+if __name__ == '__main__':
+    main()
